5f86037ff3e31c3c5929614cd7291a91f5e4bd90
sulthonmufti <msulthonmufti14@gmail.com>
2026-06-03 11:48:25 +0700

    feat: Configure environment variables and MongoDB Atlas connection

---
+++ server/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Berhasil Terhubung: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error Koneksi MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
