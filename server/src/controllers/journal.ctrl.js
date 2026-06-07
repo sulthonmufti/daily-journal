1d710921526d5a7e3c9265b93214e55769a1d106
sulthonmufti <msulthonmufti14@gmail.com>
2026-06-07 21:31:02 +0700

    feat: add update and delete journal entries

--- server/src/controllers/journal.ctrl.js
+++ server/src/controllers/journal.ctrl.js
@@ -18,4 +18,31 @@ const createEntry = async (req, res) => {
   }
 };
 
-module.exports = { getEntries, createEntry };
+const updateEntry = async (req, res) => {
+  try {
+    const entry = await JournalService.updateEntry(
+      req.user._id,
+      req.params.id,
+      req.body,
+    );
+    res.status(200).json({ success: true, data: entry });
+  } catch (error) {
+    res.status(404).json({ success: false, message: error.message });
+  }
+};
+
+const deleteEntry = async (req, res) => {
+  try {
+    await JournalService.deleteEntry(req.user._id, req.params.id);
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "Jurnal berhasil dihapus secara permanen",
+      });
+  } catch (error) {
+    res.status(404).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { getEntries, createEntry, updateEntry, deleteEntry };
--- server/src/routes/journal.routes.js
+++ server/src/routes/journal.routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getEntries, createEntry } = require("../controllers/journal.ctrl");
+const {
+  getEntries,
+  createEntry,
+  updateEntry,
+  deleteEntry,
+} = require("../controllers/journal.ctrl");
 const { protect } = require("../middleware/auth.middleware");
 const validate = require("../middleware/validate");
 const { createEntrySchema } = require("../validators/entry.schema");
@@ -12,5 +17,10 @@ router
   .route("/")
   .get(getEntries) //ambil data, gak pake Zod
   .post(validate(createEntrySchema), createEntry); // Zod dicegat sebelum masuk createEntry
+
+router
+  .route("/:id")
+  .put(validate(createEntrySchema), updateEntry)
+  .delete(deleteEntry);
 
 module.exports = router;
